7582b3b47e90a948df773dbaa0d3785961d5aa00
Tuurmeeus <50370038+Tuurmeeus@users.noreply.github.com>
2021-08-15 12:56:17 +0200

    Ophalen data api + aanmaken klasses Artikels

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
 "use strict";
+
+/**fetch('https://thecrew.cc/news/read.php')
+.then(response => response.json())
+.then(data => console.log(data));*/
+
+let artikelsList = [];
+
+fetch(`https://thecrew.cc/news/read.php`)
+.then(response => {
+    return response.json();
+})
+ .then(data => {
+    
+    for(let i = 0; i < data.news.length; i++){
+        artikelsList[i] = new Artikels(data.news[i].UUID, data.news[i].title, data.news[i].content, data.news[i].imageURI, data.news[i].likes, data.news[i].publicationDate);
+    }
+    console.log(artikelsList);
+});
+
+
+class Artikels {
+    constructor(uuid, title, content, imageURI, likes, publicationDate){
+
+        this.uuid = uuid;
+        this.title = title;
+        this.content = content;
+        this.imageURI = imageURI;
+        this.likes = likes;
+        this.datum = publicationDate;
+        
+
+    }
+}
+
+
