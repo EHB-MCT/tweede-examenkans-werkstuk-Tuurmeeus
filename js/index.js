b4650bca49034abe7c06772f434571e00d2f1ee8
Tuurmeeus <50370038+Tuurmeeus@users.noreply.github.com>
2021-08-15 16:10:06 +0200

    Ophalen data api -> in klassen opslaan

--- js/index.js
+++ js/index.js
@@ -1,23 +1,30 @@
 "use strict";
 
-/**fetch('https://thecrew.cc/news/read.php')
-.then(response => response.json())
-.then(data => console.log(data));*/
 
-let artikelsList = [];
+const artikels = {
+    articlesList: [],
+    init(){
 
-fetch(`https://thecrew.cc/news/read.php`)
-.then(response => {
-    return response.json();
-})
- .then(data => {
-    
-    for(let i = 0; i < data.news.length; i++){
-        artikelsList[i] = new Artikels(data.news[i].UUID, data.news[i].title, data.news[i].content, data.news[i].imageURI, data.news[i].likes, data.news[i].publicationDate);
+    },
+    async fetchArticle(){
+        fetch('https://thecrew.cc/news/read.php')
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            //console.log(data);
+            //for(let i = 0; i <= data.news.lenght; i++){
+            //    let article = new Artikels(data.news[i].UUID, data.news[i].title, data.news[i].content, data.news[i].imageURI, data.news[i].likes, data.news[i].publicationDate);
+            //    this.articlesList.push(article);
+            //}
+            for(let i = 0; i < data.news.length; i++){
+                let artikel = new Artikels(data.news[i].UUID, data.news[i].title, data.news[i].content, data.news[i].imageURI, data.news[i].likes, data.news[i].publicationDate);
+                this.articlesList.push(artikel);
+            }
+            console.log(this.articlesList);
+        })
     }
-    console.log(artikelsList);
-});
-
+}
 
 class Artikels {
     constructor(uuid, title, content, imageURI, likes, publicationDate){
@@ -28,9 +35,8 @@ class Artikels {
         this.imageURI = imageURI;
         this.likes = likes;
         this.datum = publicationDate;
-        
-
     }
 }
 
 
+artikels.fetchArticle();
